cc2d1b82b31ad6a314bf43f977941d8ec4be6576
Dias <diasakbergen2205@gmail.com>
2024-11-08 18:01:30 +0500

    Initial Commit

---
+++ Table View2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View2
+//
+//  Created by Диас Акберген on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
